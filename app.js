11f4fd131ba51d785066a508acecc936eaca0c46
Faouzi BOUSSAD <80760013+faouzi96@users.noreply.github.com>
2022-06-05 00:20:28 +0200

    some changes

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 http.createServer(function(request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('<html><body><h1>Hello World!</h1></body></html>');
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.end('<html><body><h1>Hello World!<br>I\'m Faouzi</h1></body></html>');
 }).listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
