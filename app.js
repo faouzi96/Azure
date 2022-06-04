aee00a2ebaa827004d39b55cfef359a4aa090620
Faouzi BOUSSAD <80760013+faouzi96@users.noreply.github.com>
2022-06-04 23:56:43 +0200

    First commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 http.createServer(function(request, response) {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World!');
+  response.end('<html><body><h1>Hello World!</h1></body></html>');
 }).listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
